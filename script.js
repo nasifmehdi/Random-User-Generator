9e2e95dea25b54b83b951149ba86f1bb1a7dbffe
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-10 20:24:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+document.querySelector('button').addEventListener('click',()=>{
+
+  fetch('https://randomuser.me/api/')
+.then(response=> {return response.json()})
+.then(data => {return (data.results[0]) })
+.then(randomUser =>{
+  document.body.style.color='black'
+  if(randomUser.gender=='female')
+  document.body.style.backgroundColor='pink'
+  else
+  document.body.style.backgroundColor='rgb(2, 124, 255)'
+  const userInfo = document.querySelector('#user')
+  let age =randomUser.dob.age
+  if(randomUser.dob.age==69){
+    age= '69 nice'
+  }
+  userInfo.innerHTML=` <div class="flex justify-between">
+  <div class="flex"> 
+    <img
+      class="w-48 h-48 rounded-full mr-8"
+      src="${randomUser.picture.large}"
+    />
+    <div class="space-y-3">
+      <p class="text-xl">
+        <span class="font-bold">Name: </span>${randomUser.name.first} ${randomUser.name.last}
+      </p>
+      <p class="text-xl">
+        <span class="font-bold">Email: </span> ${randomUser.email}
+      </p>
+      <p class="text-xl">
+        <span class="font-bold">Phone: </span> ${randomUser.phone}
+      </p>
+      <p class="text-xl">
+        <span class="font-bold">Location: </span> ${randomUser.location.city} ${randomUser.location.country}
+      </p>
+      <p class="text-xl"><span class="font-bold">Age: </span> ${age}</p>
+    </div>
+  </div>
+</div>`
+})
+
+
+})
+
+
